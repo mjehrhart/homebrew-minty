3b4733ccbe7ec5bbf3cba74eb4da3454e2e0a801
Matthew Ehrhart <matthew@Matthews-MacBook-Pro.local>
2022-04-11 10:52:38 -0700

    trying cask revert

--- Formula/minty.rb
+++ Formula/minty.rb
@@ -1,11 +1,13 @@
-cask "minty" do
-  version "5.0"
-  sha256 "26846851e699c56f82245f41f51181430d8d51b32ec36a0ddb1bfce4b65d89c3"
-
-  url "https://github.com/mjehrhart/minty/releases/download/v5.0.0/minty.zip"
-  name "Minty"
-  desc "Very fast file deduplication app written in rust using parrellism with a rust gui."
+ class Minty < Formula
+  desc "Very fast file deduplication app written in rust using parrellism with a rust gui. "
   homepage "https://github.com/mjehrhart/minty"
+  url "https://github.com/mjehrhart/minty/releases/download/v4.0.0/minty.tar.gz"
+  version "4.0.0"
+  sha256 "26846851e699c56f82245f41f51181430d8d51b32ec36a0ddb1bfce4b65d89c3"
+  license "BSD 2-Clause License"
 
-  app "Minty.app"
+  def install
+    bin.install "minty"
+  end
+
 end
