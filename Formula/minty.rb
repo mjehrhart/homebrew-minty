bada9b59d76f8bae7bde98ebd38429d059461c87
Matthew Ehrhart <matthew@Matthews-MacBook-Pro.local>
2022-04-11 10:50:00 -0700

    trying cask

--- Formula/minty.rb
+++ Formula/minty.rb
@@ -1,13 +1,11 @@
- class Minty < Formula
-  desc "Very fast file deduplication app written in rust using parrellism with a rust gui. "
-  homepage "https://github.com/mjehrhart/minty"
-  url "https://github.com/mjehrhart/minty/releases/download/v4.0.0/minty.tar.gz"
-  version "4.0.0"
+cask "minty" do
+  version "5.0"
   sha256 "26846851e699c56f82245f41f51181430d8d51b32ec36a0ddb1bfce4b65d89c3"
-  license "BSD 2-Clause License"
 
-  def install
-    bin.install "minty"
-  end
+  url "https://github.com/mjehrhart/minty/releases/download/v5.0.0/minty.zip"
+  name "Minty"
+  desc "Very fast file deduplication app written in rust using parrellism with a rust gui."
+  homepage "https://github.com/mjehrhart/minty"
 
+  app "Minty.app"
 end
