a43b9900326964f7628891f777a2942e576f37a9
Matthew Ehrhart <matthew@Matthews-MacBook-Pro.local>
2022-04-10 18:40:38 -0700

    update version 2.0

--- Formula/minty.rb
+++ Formula/minty.rb
@@ -2,8 +2,8 @@
   desc "Very fast file depublication app written in rust using parrellism with a rust gui."
   homepage "https://github.com/mjehrhart/minty"
   url "https://github.com/mjehrhart/minty/releases/download/publish/minty.tar.gz"
-  version "2.0.1"
-  sha256 "05a303e6d743fabe95022c426f40c428fc8f72e0850d96b9cd9537157cea97a1"
+  version "2.0.0"
+  sha256 "eb9fabb803e7dca0855b08d30247bc28e9fc222dc60ec579bff78459e7694cad"
   license "BSD 2-Clause License"
 
   def install
