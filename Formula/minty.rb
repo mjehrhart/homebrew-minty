44a9342a30359063cb53fa848931ac516bce0a29
Matthew Ehrhart <matthew@Matthews-MacBook-Pro.local>
2022-04-11 12:32:01 -0700

    revert

--- Formula/minty.rb
+++ Formula/minty.rb
@@ -8,18 +8,6 @@
 
   def install
     bin.install "minty"
-  end
-
-  cask "minty" do
-    version "5.0"
-    sha256 "26846851e699c56f82245f41f51181430d8d51b32ec36a0ddb1bfce4b65d89c3"
-
-    url "https://github.com/mjehrhart/minty/releases/download/v5.0.0/minty.zip"
-    name "Minty"
-    desc "Very fast file deduplication app written in rust using parrellism with a rust gui."
-    homepage "https://github.com/mjehrhart/minty"
-
-    app "Minty.app"
-  end
+  end 
 
 end
