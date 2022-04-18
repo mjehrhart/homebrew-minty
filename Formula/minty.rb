3f7dd9be34f2d7df773699b6cbbfcbe9c6df3355
Matthew Ehrhart <matthew@Matthews-MacBook-Pro.local>
2022-04-17 17:27:07 -0700

    updated shasum

--- Formula/minty.rb
+++ Formula/minty.rb
@@ -3,7 +3,7 @@
   homepage "https://github.com/mjehrhart/minty"
   url "https://github.com/mjehrhart/minty/releases/download/v2.0.0/minty.tar.gz"
   version "2.0.0"
-  sha256 "8ded6578f7517cb6f5e20ae2dc0ae1ec70d60adecebb167a61485baa7590f8ed"
+  sha256 "d5525571848e98edc32bb7f9f2d3c5291aa9b11fe0b351f23859fbfbf73a75c0"
   license "BSD 2-Clause License"
 
   def install
