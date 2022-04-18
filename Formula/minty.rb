1e7eae5cbf30e2c05d57765bf39562358e5cf4ee
Matthew Ehrhart <matthew@Matthews-MacBook-Pro.local>
2022-04-17 17:40:01 -0700

    v2.0.1

--- Formula/minty.rb
+++ Formula/minty.rb
@@ -1,9 +1,9 @@
  class Minty < Formula
   desc "Extremely fast file deduplication app written in rust using parrellism with a rust gui."
   homepage "https://github.com/mjehrhart/minty"
-  url "https://github.com/mjehrhart/minty/releases/download/v2.0.0/minty.tar.gz"
-  version "2.0.0"
-  sha256 "d5525571848e98edc32bb7f9f2d3c5291aa9b11fe0b351f23859fbfbf73a75c0"
+  url "https://github.com/mjehrhart/minty/releases/download/v2.0.1/minty.tar.gz"
+  version "2.0.1"
+  sha256 "d22500caa0aeb28b236e914b6f810b7c07ec67b3949893bf713f5feefc09b9f3"
   license "BSD 2-Clause License"
 
   def install
